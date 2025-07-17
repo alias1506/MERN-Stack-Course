8e486a68f7e8d331feaf480bbaebc287f17c1d69
Gourab Majumdar <gourabjr1234@gmail.com>
2025-07-18 01:36:58 +0530

    Day 9 - Calculator

--- Day_9_17-07-2025/Calculator/assets/js/script.js
+++ Day_9_17-07-2025/Calculator/assets/js/script.js
@@ -3,31 +3,66 @@ let decimal = document.getElementById("decimal");
 let result = 0;
 
 function calcfunc(action) {
-  if (action == "ac") {
-    document.getElementById("screen").value = "";
-    decimal.disabled = false;
-  } else if (action == "c") {
-    let val = input.value;
-    let newVal = val.slice(0, -1);
-    val = newVal;
-    input.value = val;
-  } else if (
-    action == "1" ||
-    action == "1" ||
-    action == "2" ||
-    action == "3" ||
-    action == "4" ||
-    action == "5" ||
-    action == "6" ||
-    action == "7" ||
-    action == "8" ||
-    action == "9" ||
-    action == "0" ||
-    action == "00"
-  ) {
-    input.value += action;
-  } else if (action == ".") {
-    input.value += action;
-    decimal.disabled = true;
+  switch (action) {
+    case "ac":
+      document.getElementById("screen").value = "";
+      decimal.disabled = false;
+      break;
+
+    case "c":
+      let val = input.value;
+      let newVal = val.slice(0, -1);
+      val = newVal;
+      input.value = val;
+      if (!input.value.includes(".")) {
+        decimal.disabled = false;
+      }
+      break;
+
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+    case "00":
+      input.value += action;
+      break;
+
+    case ".":
+      if (!getLastNumber(input.value).includes(".")) {
+        input.value += action;
+      }
+      break;
+
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      input.value += action;
+      decimal.disabled = false;
+      break;
+
+    case "%":
+      input.value = input.value / 100;
+      decimal.disabled = false;
+      break;
+
+    case "=":
+      input.value = eval(input.value);
+      decimal.disabled = false;
+      break;
+
+    default:
+      break;
   }
+}
+
+function getLastNumber(expression) {
+  const parts = expression.split(/[\+\-\*\/]/);
+  return parts[parts.length - 1];
 }
