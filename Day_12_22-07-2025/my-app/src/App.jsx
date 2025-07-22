94ee6078f8e7b8c9b1e5e21923a225f6f78a05d7
Gourab Majumdar <gourabjr1234@gmail.com>
2025-07-22 16:44:53 +0530

    Day 12 (React Started)

---
+++ Day_12_22-07-2025/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Demo from './components/demo.jsx'
+import Profile from './components/profile.jsx'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world!!!</h1>
+      <Demo />
+      <Profile />
+    </div>
+  )
+}
+
+export default App
---
+++ Day_12_22-07-2025/my-app/src/components/demo.jsx
@@ -0,0 +1,11 @@
+import './demo.css'
+export default function Demo() {
+    return (
+        <div className='container'>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus animi libero id porro placeat maiores ipsa voluptatibus provident quisquam cupiditate quam molestiae quis, assumenda enim expedita, quae, deleniti eaque facilis.
+            </p>
+            <button className='btn btn-primary'>Click</button>
+        </div>
+    )
+}
